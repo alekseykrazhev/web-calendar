048305b7cd9ef7f15be02570619b2fa8d91f1a58
alekseykrazhev <alekseykrazhev@yandex.by>
2022-09-14 14:48:25 +0300

    Start working on authentication.

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from datetime import datetime
-from flask import Flask, jsonify, request, abort, render_template, redirect
+from flask import Flask, request, abort, render_template
 from flask_cors import CORS
 from flask_restful import reqparse, inputs
 from flask_sqlalchemy import SQLAlchemy
-from module import check_login, login
 import sys
 
 app = Flask(__name__)
@@ -11,7 +10,6 @@ CORS(app)
 db = SQLAlchemy(app)
 parser = reqparse.RequestParser()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
-is_logged_in = False
 
 parser.add_argument("event",
                     type=str,
@@ -32,21 +30,12 @@ class EventInfo(db.Model):
 
 @app.route('/')
 def main_page():
-    if check_login(is_logged_in):
-        return check_login()
 
     return render_template('main.html'), 200
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login_page():
-    return render_template('login.html'), 200
-
-
 @app.route('/event', methods=['GET', 'POST'])
 def event_page():
-    if check_login(is_logged_in):
-        return check_login()
 
     if request.method == 'GET':
         pass
@@ -63,49 +52,54 @@ def event_page():
     args = request.args
     start_time = args.get('start_time')
     end_time = args.get('end_time')
+
     if start_time and end_time:
         events = EventInfo.query.filter(EventInfo.date >= start_time, EventInfo.date <= end_time).all()
     else:
         events = EventInfo.query.all()
     ans = []
+
     for event in events:
         ans.append({'id': event.id, 'event': event.event, 'date': event.date.strftime('%Y-%m-%d')})
+
     return render_template('event.html', events=ans), 200
 
 
 @app.route('/event/today')
 def today_event():
-    global is_logged_in
-    is_logged_in = login(is_logged_in)
-    if check_login(is_logged_in):
-        return check_login()
 
     today = datetime.now().date()
     events = EventInfo.query.filter_by(date=today).all()
     ans = []
+
     if events:
         for event in events:
             ans.append({'id': event.id, 'event': event.event, 'date': event.date.strftime('%Y-%m-%d')})
         # return ans
+
     return render_template('today.html', events=ans), 200
 
 
 @app.route('/event/<int:event_id>', methods=['GET', 'DELETE'])
 def event_by_id(event_id):
-    if check_login(is_logged_in):
-        return check_login()
 
     if request.method == 'GET':
         event = EventInfo.query.filter(EventInfo.id == event_id).first()
+
         if event is None:
             abort(404, "The event doesn't exist!")
+
         return render_template('event_by_id.html', event=event), 200
+
     elif request.method == 'DELETE':
         event = EventInfo.query.filter(EventInfo.id == event_id).first()
+
         if event is None:
             abort(404, "The event doesn't exist!")
+
         db.session.delete(event)
         db.session.commit()
+
         return 'The event has been deleted!', 200
 
 
