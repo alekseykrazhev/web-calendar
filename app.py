338328e32db621ae390aed362b8011962602f888
alekseykrazhev <alekseykrazhev@yandex.by>
2022-09-14 22:29:21 +0300

    authentication added

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
+import secrets
 from datetime import datetime
-from flask import Flask, request, abort, render_template
+
+from flask import Flask, request, abort, render_template, redirect
 from flask_cors import CORS
+from flask_login import login_required, current_user, login_user, logout_user
 from flask_restful import reqparse, inputs
-from flask_sqlalchemy import SQLAlchemy
-import sys
+
+from models import EventInfo, database, UserModel, login
 
 app = Flask(__name__)
-CORS(app)
-db = SQLAlchemy(app)
+secret_key = secrets.token_urlsafe(32)
+app.secret_key = secret_key
+
 parser = reqparse.RequestParser()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+database.init_app(app)
+login.init_app(app)
+CORS(app)
 
 parser.add_argument("event",
                     type=str,
@@ -19,24 +28,59 @@ parser.add_argument("date",
                     type=inputs.date,
                     help="The event date with the correct format is required! The correct format is YYYY-MM-DD!",
                     required=True)
+'''
+Authentication block
+
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, LoginManager
+
+database = SQLAlchemy(app)
+login = LoginManager(app)
 
 
-class EventInfo(db.Model):
+class EventInfo(database.Model):
     __tablename__ = 'events'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    event = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.Date, nullable=False)
+
+    id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    event = database.Column(database.String(100), nullable=False)
+    date = database.Column(database.Date, nullable=False)
+
+
+class UserModel(UserMixin, database.Model):
+    __tablename__ = 'users'
+
+    id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    email = database.Column(database.String(80), unique=True)
+    username = database.Column(database.String(100))
+    password_hash = database.Column(database.String())
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return UserModel.query.get(int(id))
+
+
+
+end block
+'''
 
 
 @app.route('/')
+@login_required
 def main_page():
-
     return render_template('main.html'), 200
 
 
 @app.route('/event', methods=['GET', 'POST'])
+@login_required
 def event_page():
-
     if request.method == 'GET':
         pass
 
@@ -46,8 +90,8 @@ def event_page():
         ei = EventInfo()
         ei.event = event
         ei.date = datetime.strptime(date, '%Y-%m-%d')
-        db.session.add(ei)
-        db.session.commit()
+        database.session.add(ei)
+        database.session.commit()
 
     args = request.args
     start_time = args.get('start_time')
@@ -66,8 +110,8 @@ def event_page():
 
 
 @app.route('/event/today')
+@login_required
 def today_event():
-
     today = datetime.now().date()
     events = EventInfo.query.filter_by(date=today).all()
     ans = []
@@ -81,8 +125,8 @@ def today_event():
 
 
 @app.route('/event/<int:event_id>', methods=['GET', 'DELETE'])
+@login_required
 def event_by_id(event_id):
-
     if request.method == 'GET':
         event = EventInfo.query.filter(EventInfo.id == event_id).first()
 
@@ -95,17 +139,59 @@ def event_by_id(event_id):
         event = EventInfo.query.filter(EventInfo.id == event_id).first()
 
         if event is None:
-            abort(404, "The event doesn't exist!")
+            abort(404, "Error 404: The event doesn't exist!")
 
-        db.session.delete(event)
-        db.session.commit()
+        database.session.delete(event)
+        database.session.commit()
 
         return 'The event has been deleted!', 200
 
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('/')
+
+    if request.method == 'POST':
+        email = request.form['email']
+        user = UserModel.query.filter_by(email=email).first()
+        if user is not None and user.check_password(request.form['password']):
+            login_user(user)
+
+            return redirect('/')
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if current_user.is_authenticated:
+        return redirect('/')
+
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+
+        if UserModel.query.filter_by(email=email).first():
+            return 'Email is already in use'
+
+        user = UserModel(email=email, username=username)
+        user.set_password(password)
+        database.session.add(user)
+        database.session.commit()
+
+        return redirect('/login')
+
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+
+    return redirect('/')
+
+
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        arg_host, arg_port = sys.argv[1].split(':')
-        app.run(host=arg_host, port=arg_port)
-    else:
-        app.run()
+    app.run(host='localhost', port=5000, debug=True)
--- models.py
+++ models.py
@@ -1,1 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, LoginManager
 
+
+database = SQLAlchemy()
+login = LoginManager()
+
+
+class EventInfo(database.Model):
+    __tablename__ = 'events'
+
+    id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    event = database.Column(database.String(100), nullable=False)
+    date = database.Column(database.Date, nullable=False)
+
+
+class UserModel(UserMixin, database.Model):
+    __tablename__ = 'users'
+
+    id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    email = database.Column(database.String(80), unique=True)
+    username = database.Column(database.String(100))
+    password_hash = database.Column(database.String())
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return UserModel.query.get(int(id))
